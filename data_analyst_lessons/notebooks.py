f6a568bc1e44bff6c45ed33a35f1344c5ab2bffb
Darlan Tane <112576210+darlantane@users.noreply.github.com>
2022-09-24 20:52:24 +0200

    notebook

--- data_analyst_lessons/notebooks.py
+++ data_analyst_lessons/notebooks.py
@@ -12,7 +12,20 @@ sales.head()
 sales['Customer_Age'].mean()
 
 sales['Customer_Age'].plot(kind='box', vert=False, figsize=(14,6))
+
 sales['Order_Quantity'].mean()
 sales['Order_Quantity'].plot(kind='hist', bins=30, figsize=(14,6))
 sales['Order_Quantity'].plot(kind='box', vert=False, figsize=(14,6))
+
 sales['Year'].value_counts()
+sales['Year'].value_counts().plot(kind='pie', figsize=(6,6))
+
+sales['Country'].value_counts().head(1)
+sales['Country'].value_counts()
+sales['Country'].value_counts().plot(kind='bar', figsize=(14,6))
+
+sales['Product'].unique()
+sales['Product'].value_counts().head(10).plot(kind='bar', figsize=(14,6))
+
+sales.plot(kind='scatter', x='Unit_Cost', y='Unit_Price', figsize=(6,6))
+sales.plot(kind='scatter', x='Order_Quantity', y='Profit', figsize=(6,6))
