0572a7a2a108e374767dd58b3402e8cea1151850
Darlan Tane <112576210+darlantane@users.noreply.github.com>
2022-09-24 20:46:57 +0200

    Fix notebook

--- data_analyst_lessons/notebooks.py
+++ data_analyst_lessons/notebooks.py
@@ -1,9 +1,18 @@
-import matplotlib
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import inline
+import matplotlib
 
 sales = pd.read_csv(
     'C:/Users/darla/Desktop/notebook/FreeCodeCamp-Pandas-Real-Life-Example-master/data/sales_data.csv',
     parse_dates=['Date'])
 sales.head()
+
+sales['Customer_Age'].mean()
+
+sales['Customer_Age'].plot(kind='box', vert=False, figsize=(14,6))
+sales['Order_Quantity'].mean()
+sales['Order_Quantity'].plot(kind='hist', bins=30, figsize=(14,6))
+sales['Order_Quantity'].plot(kind='box', vert=False, figsize=(14,6))
+sales['Year'].value_counts()
