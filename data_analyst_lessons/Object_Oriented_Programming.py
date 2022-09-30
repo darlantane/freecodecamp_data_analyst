4e7bd931ccee929ff05dfaf218ae83d0b7e2b325
Darlan Tane <112576210+darlantane@users.noreply.github.com>
2022-09-30 22:57:59 +0200

    Object_Oriented_Programming

--- data_analyst_lessons/Object_Oriented_Programming.py
+++ data_analyst_lessons/Object_Oriented_Programming.py
@@ -17,3 +17,26 @@ class vehicule:
 voiture_1 = vehicule(couleur='rouge')
 voiture_1.accelerer(100)
 voiture_1.afficher()
+
+class voiture_electrique(vehicule):
+    """
+    La classe moto hérite des méthodes et des attributs de la classe véhicule
+    """
+
+    def __init__(self, couleur='black', vitesse=0, roues=4, autonomie=100):
+        super().__init__(couleur, vitesse, roues) # super() permet d'utiliser la fonction de la classe "parent"
+        self.autonomie = autonomie
+
+    # Ré-écriture de certaines méthodes
+    def accelerer(self, vitesse):
+        super().accelerer(vitesse)
+        self.autonomie -= 0.1 *self.vitesse
+
+    def afficher(self):
+        super().afficher()
+        print(f'autonomie: {self.autonomie}')
+voiture_2 = voiture_electrique()
+voiture_2.afficher()
+
+voiture_2.accelerer(10)
+voiture_2.afficher()
