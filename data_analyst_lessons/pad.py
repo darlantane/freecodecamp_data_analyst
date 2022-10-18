c43663043c311a9e7974633bf3dcf1db9f0954c4
Darlan Tane <112576210+darlantane@users.noreply.github.com>
2022-10-18 06:14:55 +0200

    pandas bases exercise

--- data_analyst_lessons/pad.py
+++ data_analyst_lessons/pad.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-data = pd.read_excel('titanic.xls')
+data = pd.read_excel('C:/Users/darla/PycharmProjects/freecodecamp_data_analyst/data_analyst_lessons/titanic.xls')
 data.shape
 data.head()
 
@@ -18,6 +18,19 @@ data.groupby(['sex']).mean()
 data['pclass'].value_counts()
 
 data[data['age'] < 18]['pclass'].value_counts()
+
+def category_ages(age):
+    if age <= 20:
+        return '<20 ans'
+    elif (age > 20) & (age <= 30):
+        return '20-30 ans'
+    elif (age > 30) & (age <= 40):
+        return '30-40 ans'
+    else:
+        return '+40 ans'
+
+data['age'] = data['age'].map(category_ages)
+data['sex'].astype('category').cat.codes
 
 
 
