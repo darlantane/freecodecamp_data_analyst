b240c12158ecea5f4b50c83936e445f2f5558247
Darlan Tane <112576210+darlantane@users.noreply.github.com>
2022-10-21 21:47:43 +0200

    Sklearn normalisation

--- data_analyst_lessons/sk_pre_processing.py
+++ data_analyst_lessons/sk_pre_processing.py
@@ -10,3 +10,35 @@ encoder.inverse_transform(np.array([0, 0, 2]))
 
 encoder = LabelBinarizer()
 encoder.fit_transform(y)
+
+X = np.array([['chat', 'poils'],
+              ['chien', 'poils'],
+              ['chat', 'poils'],
+              ['oiseau', 'plumes']])
+
+encoder = OrdinalEncoder()
+encoder.fit_transform(X)
+
+encoder = OneHotEncoder(sparse=False)
+encoder.fit_transform(X)
+
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler
+from sklearn.datasets import load_iris
+
+iris = load_iris()
+X = iris.data
+
+X_minmax = MinMaxScaler().fit_transform(X)
+
+plt.scatter(X[:, 2], X[:, 3])
+plt.scatter(X_minmax[:, 2], X_minmax[:, 3])
+
+X_stdscl = StandardScaler().fit_transform(X)
+
+plt.scatter(X[:, 2], X[:, 3])
+plt.scatter(X_stdscl[:, 2], X_stdscl[:, 3])
+
+X_robust = RobustScaler().fit_transform(X)
+
+plt.scatter(X[:, 2], X[:, 3])
+plt.scatter(X_robust[:, 2], X_robust[:, 3])
