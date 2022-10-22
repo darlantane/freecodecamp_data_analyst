42810e7472490e02111ba43b1aa34923582b77f6
Darlan Tane <112576210+darlantane@users.noreply.github.com>
2022-10-22 19:43:34 +0200

    Sklearn SelectKBest

--- data_analyst_lessons/sk_feature_selection.py
+++ data_analyst_lessons/sk_feature_selection.py
@@ -22,3 +22,12 @@ selector.get_support()
 np.array(iris.feature_names)[selector.get_support()]
 
 selector.variances_
+
+from sklearn.feature_selection import SelectKBest, chi2, f_classif
+chi2(X, y)
+
+selector = SelectKBest(f_classif, k=2)
+selector.fit(X, y)
+selector.scores_
+
+np.array(iris.feature_names)[selector.get_support()]
