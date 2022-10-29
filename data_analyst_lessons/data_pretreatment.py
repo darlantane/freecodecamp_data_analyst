c5da5201bb684a2ffda207354e8d3b7b0c24f9b8
Darlan Tane <112576210+darlantane@users.noreply.github.com>
2022-10-29 20:52:22 +0200

    data_pretreatment traintest

--- data_analyst_lessons/data_pretreatment.py
+++ data_analyst_lessons/data_pretreatment.py
@@ -16,3 +16,45 @@ viral_columns = list(df.columns[(missing_rate < 0.80) & (missing_rate > 0.75)])
 key_columns = ['Patient age quantile', 'SARS-Cov-2 exam result']
 df = df[key_columns + blood_columns + viral_columns]
 df.head()
+
+from sklearn.model_selection import train_test_split
+trainset, testset = train_test_split(df, test_size=0.2, random_state=0)
+trainset['SARS-Cov-2 exam result'].value_counts()
+
+testset['SARS-Cov-2 exam result'].value_counts()
+
+def encodage(df):
+    code = {'negative':0,
+            'positive':1,
+            'not_detected':0,
+            'detected':1}
+
+    for col in df.select_dtypes('object').columns:
+        df.loc[:,col] = df[col].map(code)
+
+    return df
+
+def feature_engineering(df):
+    df['est malade'] = df[viral_columns].sum(axis=1) >= 1
+    df = df.drop(viral_columns, axis=1)
+    return df
+def imputation(df):
+    #df['is na'] = (df['Parainfluenza 3'].isna()) | (df['Leukocytes'].isna())
+    #df = df.fillna(-999)
+    df = df.dropna(axis=0)
+    return  df
+def preprocessing(df):
+
+    df = encodage(df)
+    df = feature_engineering(df)
+    df = imputation(df)
+
+    X = df.drop('SARS-Cov-2 exam result', axis=1)
+    y = df['SARS-Cov-2 exam result']
+
+    print(y.value_counts())
+
+    return X, y
+X_train, y_train = preprocessing(trainset)
+
+X_test, y_test = preprocessing(testset)
