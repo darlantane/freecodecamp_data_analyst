f4324e93022d09974472c5a6b4d9811fa4fd8b13
Darlan Tane <112576210+darlantane@users.noreply.github.com>
2022-10-30 22:13:47 +0100

    data pretreatement evaluation

--- data_analyst_lessons/data_pretreatment.py
+++ data_analyst_lessons/data_pretreatment.py
@@ -70,3 +70,25 @@ model_1 = RandomForestClassifier(random_state=0)
 model_2 = make_pipeline(PolynomialFeatures(2), SelectKBest(f_classif, k=10),
                         RandomForestClassifier(random_state=0))
 
+from sklearn.metrics import f1_score, confusion_matrix, classification_report
+from sklearn.model_selection import learning_curve
+def evaluation(model):
+
+    model.fit(X_train, y_train)
+    ypred = model.predict(X_test)
+
+    print(confusion_matrix(y_test, ypred))
+    print(classification_report(y_test, ypred))
+
+    N, train_score, val_score = learning_curve(model, X_train, y_train,
+                                               cv=4, scoring='f1',
+                                               train_sizes=np.linspace(0.1, 1, 10))
+
+
+    plt.figure(figsize=(12, 8))
+    plt.plot(N, train_score.mean(axis=1), label='train score')
+    plt.plot(N, val_score.mean(axis=1), label='validation score')
+    plt.legend()
+
+
+evaluation(model_1)
