004e9aee2004c7025943fc98fca077ee3755a04f
Darlan Tane <112576210+darlantane@users.noreply.github.com>
2022-11-03 21:34:56 +0100

    Fix Pipelines composites

--- data_analyst_lessons/pipeline_composites.py
+++ data_analyst_lessons/pipeline_composites.py
@@ -15,3 +15,14 @@ y = titanic['survived']
 X = titanic.drop('survived', axis=1)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+numerical_features = ['pclass', 'age', 'fare']
+categorical_features = ['sex', 'deck', 'alone']
+numerical_pipeline = make_pipeline(SimpleImputer(strategy='mean'), StandardScaler())
+categorical_pipeline = make_pipeline(SimpleImputer(strategy='most_frequent'), OneHotEncoder())
+preprocessor = make_column_transformer((numerical_pipeline, numerical_features),
+                                       (categorical_pipeline, categorical_features))
+model = make_pipeline(preprocessor, SGDClassifier())
+
+model.fit(X_train, y_train)
+model.score(X_test, y_test)
