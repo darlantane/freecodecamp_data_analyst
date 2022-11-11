8307ff7cb2230ab8b167aca695341dbf40d3bcb8
Darlan Tane <112576210+darlantane@users.noreply.github.com>
2022-11-11 20:33:12 +0100

    linear model

--- data_analyst_lessons/multiple_linear_regression.py
+++ data_analyst_lessons/multiple_linear_regression.py
@@ -23,3 +23,46 @@ print(X[:10])
 
 theta = np.random.randn(3, 1)
 theta
+
+def model(X, theta):
+    return X.dot(theta)
+
+plt.scatter(x, y)
+plt.scatter(x, model(X, theta), c='r')
+
+def cost_function(X, y, theta):
+    m = len(y)
+    return 1/(2*m) * np.sum((model(X, theta) - y)**2)
+cost_function(X, y, theta)
+
+def grad(X, y, theta):
+    m = len(y)
+    return 1/m * X.T.dot(model(X, theta) - y)
+def gradient_descent(X, y, theta, learning_rate, n_iterations):
+
+    cost_history = np.zeros(n_iterations)
+
+    for i in range(0, n_iterations):
+        theta = theta - learning_rate * grad(X, y, theta)
+        cost_history[i] = cost_function(X, y, theta)
+
+    return theta, cost_history
+
+n_iterations = 1000
+learning_rate = 0.01
+
+theta_final, cost_history = gradient_descent(X, y, theta, learning_rate, n_iterations)
+theta_final
+
+predictions = model(X, theta_final)
+
+plt.scatter(x, y)
+plt.scatter(x, predictions, c='r')
+
+plt.plot(range(n_iterations), cost_history)
+
+def coef_determination(y, pred):
+    u = ((y - pred)**2).sum()
+    v = ((y - y.mean())**2).sum()
+    return 1 - u/v
+coef_determination(y, predictions)
