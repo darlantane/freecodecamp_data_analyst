35767721459ee66b3fc37b0689d4c165d7d0d021
Darlan Tane <112576210+darlantane@users.noreply.github.com>
2022-11-12 21:30:39 +0100

    dataset

--- data_analyst_lessons/multiple_linear_regression.py
+++ data_analyst_lessons/multiple_linear_regression.py
@@ -66,3 +66,35 @@ def coef_determination(y, pred):
     v = ((y - y.mean())**2).sum()
     return 1 - u/v
 coef_determination(y, predictions)
+
+np.random.seed(0)
+
+x, y = make_regression(n_samples=100, n_features=2, noise = 10)
+
+plt.scatter(x[:,0], y)
+
+from mpl_toolkits.mplot3d import Axes3D
+#%matplotlib notebook #activez cette ligne pour manipuler le graph 3D
+
+ax = fig.add_subplot(111, projection='3d')
+
+ax.scatter(x[:,0], x[:,1], y)
+
+ax.set_xlabel('x_1')
+ax.set_ylabel('x_2')
+ax.set_zlabel('y')
+
+# Verification des dimensions
+print(x.shape)
+print(y.shape)
+
+y = y.reshape(y.shape[0], 1)
+print(y.shape)
+
+X = np.hstack((x, np.ones((x.shape[0], 1)))) # ajoute un vecteur Biais de dimension (x.shape[0], 1)
+
+print(X.shape)
+print(X[:10])
+
+theta = np.random.randn(3, 1)
+theta
