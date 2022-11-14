c9765d388d2bb7b9fa7438b9482dcfc8dcffe2b4
Darlan Tane <112576210+darlantane@users.noreply.github.com>
2022-11-14 22:09:07 +0100

    gradients

--- data_analyst_lessons/logistic_regression.py
+++ data_analyst_lessons/logistic_regression.py
@@ -16,3 +16,10 @@ def forward_propagation(X, W, b):
     Z = X.dot(W) + b
     A = sigmoid(Z)
     return A
+
+def log_loss(y, A):
+    return 1/len(y) * np.sum(-y * np.log(A) - (1 - y) * np.log(1 - A))
+def gradients(X, A, y):
+    dW = 1/len(y) * np.dot(X.T, A - y)
+    db = 1/len(y) * np.sum(A - y)
+    return (dW, db)
