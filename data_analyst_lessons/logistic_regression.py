2542ceeb38a80a1c91c3e8064e5bce02e7457e7a
Darlan Tane <112576210+darlantane@users.noreply.github.com>
2022-11-14 22:06:02 +0100

    logistic regression

---
+++ data_analyst_lessons/logistic_regression.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
+
+X, y = make_blobs(n_samples=100, n_features=2, centers=2, random_state=0)
+y = y.reshape((y.shape[0], 1))
+plt.scatter(X[:,0], X[:, 1], c=y, cmap='bwr')
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+def initialisation(X):
+    W = np.random.randn(X.shape[1], 1)
+    b = np.random.randn(1)
+    return (W, b)
+def forward_propagation(X, W, b):
+    Z = X.dot(W) + b
+    A = sigmoid(Z)
+    return A
