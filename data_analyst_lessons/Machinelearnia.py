b9e581ac00f4d11254091bbe0d7d02cf4b42dc99
Darlan Tane <112576210+darlantane@users.noreply.github.com>
2022-11-19 18:52:58 +0100

    dataset

--- data_analyst_lessons/Machinelearnia.py
+++ data_analyst_lessons/Machinelearnia.py
@@ -10,3 +10,18 @@ print('dimensions de y:', y.shape)
 
 plt.scatter(X[:,0], X[:, 1], c=y, cmap='summer')
 plt.show()
+
+def initialisation(X):
+    W = np.random.randn(X.shape[1], 1)
+    b = np.random.randn(1)
+    return (W, b)
+def model(X, W, b):
+    Z = X.dot(W) + b
+    A = 1 / (1 + np.exp(-Z))
+    return A
+def log_loss(A, y):
+    return 1 / len(y) * np.sum(-y * np.log(A) - (1 - y) * np.log(1 - A))
+def gradients(A, X, y):
+    dW = 1 / len(y) * np.dot(X.T, A - y)
+    db = 1 / len(y) * np.sum(A - y)
+    return (dW, db)
