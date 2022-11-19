40b7d128bfd87026f4ef1b47e0428105b88e3b28
Darlan Tane <112576210+darlantane@users.noreply.github.com>
2022-11-19 18:56:14 +0100

    Fonctions

--- data_analyst_lessons/Machinelearnia.py
+++ data_analyst_lessons/Machinelearnia.py
@@ -25,3 +25,34 @@ def gradients(A, X, y):
     dW = 1 / len(y) * np.dot(X.T, A - y)
     db = 1 / len(y) * np.sum(A - y)
     return (dW, db)
+
+def update(dW, db, W, b, learning_rate):
+    W = W - learning_rate * dW
+    b = b - learning_rate * db
+    return (W, b)
+def predict(X, W, b):
+    A = model(X, W, b)
+    # print(A)
+    return A >= 0.5
+from sklearn.metrics import accuracy_score
+def artificial_neuron(X, y, learning_rate = 0.1, n_iter = 100):
+
+    W, b = initialisation(X)
+
+    Loss = []
+
+    for i in range(n_iter):
+        A = model(X, W, b)
+        Loss.append(log_loss(A, y))
+        dW, db = gradients(A, X, y)
+        W, b = update(dW, db, W, b, learning_rate)
+
+    y_pred = predict(X, W, b)
+    print(accuracy_score(y, y_pred))
+
+    plt.plot(Loss)
+    plt.show()
+
+    return (W, b)
+
+W, b = artificial_neuron(X, y)
