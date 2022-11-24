f42c379944608a502de8530a7e04dc6c5d46ede6
Darlan Tane <112576210+darlantane@users.noreply.github.com>
2022-11-24 21:52:22 +0100

    Visualisation

--- data_analyst_lessons/Machinelearnia.py
+++ data_analyst_lessons/Machinelearnia.py
@@ -83,3 +83,18 @@ fig = go.Figure(data=[go.Scatter3d(
 fig.update_layout(template= "plotly_dark", margin=dict(l=0, r=0, b=0, t=0))
 fig.layout.scene.camera.projection.type = "orthographic"
 fig.show()
+
+X0 = np.linspace(X[:, 0].min(), X[:, 0].max(), 100)
+X1 = np.linspace(X[:, 1].min(), X[:, 1].max(), 100)
+xx0, xx1 = np.meshgrid(X0, X1)
+Z = W[0] * xx0 + W[1] * xx1 + b
+A = 1 / (1 + np.exp(-Z))
+
+fig = (go.Figure(data=[go.Surface(z=A, x=xx0, y=xx1, colorscale='YlGn', opacity = 0.7, reversescale=True)]))
+
+fig.add_scatter3d(x=X[:, 0].flatten(), y=X[:, 1].flatten(), z=y.flatten(), mode='markers', marker=dict(size=5, color=y.flatten(), colorscale='YlGn', opacity = 0.9, reversescale=True))
+
+
+fig.update_layout(template= "plotly_dark", margin=dict(l=0, r=0, b=0, t=0))
+fig.layout.scene.camera.projection.type = "orthographic"
+fig.show()
