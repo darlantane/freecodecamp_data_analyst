a884c3c98fd11d9656733bf6ffedf03bf39e51c5
Darlan Tane <112576210+darlantane@users.noreply.github.com>
2022-11-24 22:04:27 +0100

    catdog

---
+++ data_analyst_lessons/catdog.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
+from sklearn.metrics import accuracy_score
+
+
+plt.style.use('dark_background')
+plt.rcParams.update({
+    "figure.facecolor":  (0.12 , 0.12, 0.12, 1),
+    "axes.facecolor": (0.12 , 0.12, 0.12, 1),
+})
