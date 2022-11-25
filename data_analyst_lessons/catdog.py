498fe994e2ebb106df6bd02d0fc63df9ca294920
Darlan Tane <112576210+darlantane@users.noreply.github.com>
2022-11-25 23:44:34 +0100

    Fonctions

--- data_analyst_lessons/catdog.py
+++ data_analyst_lessons/catdog.py
@@ -9,3 +9,32 @@ plt.rcParams.update({
     "figure.facecolor":  (0.12 , 0.12, 0.12, 1),
     "axes.facecolor": (0.12 , 0.12, 0.12, 1),
 })
+
+
+def initialisation(X):
+    W = np.random.randn(X.shape[1], 1)
+    b = np.random.randn(1)
+    return (W, b)
+def model(X, W, b):
+    Z = X.dot(W) + b
+    # print(Z.min())
+    A = 1 / (1 + np.exp(-Z))
+    return A
+def log_loss(A, y):
+    epsilon = 1e-15
+    return 1 / len(y) * np.sum(-y * np.log(A + epsilon) - (1 - y) * np.log(1 - A + epsilon))
+
+def gradients(A, X, y):
+    dW = 1 / len(y) * np.dot(X.T, A - y)
+    db = 1 / len(y) * np.sum(A - y)
+    return (dW, db)
+
+def update(dW, db, W, b, learning_rate):
+    W = W - learning_rate * dW
+    b = b - learning_rate * db
+    return (W, b)
+def predict(X, W, b):
+    A = model(X, W, b)
+    # print(A)
+    return A >= 0.5
+from tqdm import tqdm
