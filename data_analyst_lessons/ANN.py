407ed98f81aa04457d1d77badf54f384153e27e1
Darlan Tane <112576210+darlantane@users.noreply.github.com>
2022-12-02 23:16:59 +0100

    ANN

---
+++ data_analyst_lessons/ANN.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs, make_circles
+from sklearn.metrics import accuracy_score, log_loss
+from tqdm import tqdm
+
+def initialisation(n0, n1, n2):
+
+    W1 = np.random.randn(n1, n0)
+    b1 = np.zeros((n1, 1))
+    W2 = np.random.randn(n2, n1)
+    b2 = np.zeros((n2, 1))
+
+    parametres = {
+        'W1': W1,
+        'b1': b1,
+        'W2': W2,
+        'b2': b2
+    }
+
+    return parametres
