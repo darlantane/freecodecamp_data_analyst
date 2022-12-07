13cd5fde1d26eab2cfd8bce7686b635c8a2dc57f
Darlan Tane <112576210+darlantane@users.noreply.github.com>
2022-12-07 22:15:36 +0100

    fonctions

--- data_analyst_lessons/ANN_profond.py
+++ data_analyst_lessons/ANN_profond.py
@@ -16,3 +16,35 @@ def initialisation(dimensions):
         parametres['b' + str(c)] = np.random.randn(dimensions[c], 1)
 
     return parametres
+
+def forward_propagation(X, parametres):
+
+    activations = {'A0': X}
+
+    C = len(parametres) // 2
+
+    for c in range(1, C + 1):
+
+        Z = parametres['W' + str(c)].dot(activations['A' + str(c - 1)]) + parametres['b' + str(c)]
+        activations['A' + str(c)] = 1 / (1 + np.exp(-Z))
+
+    return activations
+
+
+
+
+def back_propagation(y, parametres, activations):
+
+    m = y.shape[1]
+    C = len(parametres) // 2
+
+    dZ = activations['A' + str(C)] - y
+    gradients = {}
+
+    for c in reversed(range(1, C + 1)):
+        gradients['dW' + str(c)] = 1/m * np.dot(dZ, activations['A' + str(c - 1)].T)
+        gradients['db' + str(c)] = 1/m * np.sum(dZ, axis=1, keepdims=True)
+        if c > 1:
+            dZ = np.dot(parametres['W' + str(c)].T, dZ) * activations['A' + str(c - 1)] * (1 - activations['A' + str(c - 1)])
+
+    return gradients
