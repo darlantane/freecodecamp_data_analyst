9acb3dcbad91259b866d44c4fa03a886f88967e3
Darlan Tane <112576210+darlantane@users.noreply.github.com>
2023-01-01 22:18:53 +0100

    Tensorflow

---
+++ data_analyst_lessons/Tensorflow.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow import keras
+
+
+(X_train, y_train) , (X_test, y_test) = keras.datasets.mnist.load_data()
+
+print('trainset:', X_train.shape)
+print('testset:', X_test.shape)
+
+
+X_train = X_train / 255
+X_test = X_test / 255
+
+fig, ax = plt.subplots(nrows=1, ncols=10, figsize=(20, 4))
+for i in range(10):
+    ax[i].imshow(X_train[i], cmap='gray')
+
+plt.tight_layout()
+plt.show()
